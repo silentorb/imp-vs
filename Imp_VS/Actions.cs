5294e203fb56c361794aa204bc19c9ea889a223b
cj <christopherwjohnson@gmail.com>
2015-04-25 14:55:15 -0600

    Also added basic imp compliation integration.

--- Imp_VS/Actions.cs
+++ Imp_VS/Actions.cs
@@ -5,33 +5,79 @@ using System.Linq;
 using System.Text;
 using EnvDTE;
 using EnvDTE80;
+using imperative;
+using metahub.render;
+using metahub.render.targets;
+using Newtonsoft.Json;
 
 namespace Imp_VS
 {
+    internal class Target
+    {
+        public string path;
+        public string type;
+    }
+
+    internal class Project_Configuration
+    {
+        public string[] sources;
+        public Target[] targets;
+    }
+
     internal static class Actions
     {
         public static void create_project_item(DTE2 application)
         {
             var solution = (Solution2)application.Solution;
-            var gen_folders = new List<ProjectItem>();
+            var imp_configs = new List<ProjectItem>();
             
             foreach (Project project in solution.Projects)
             {
                 foreach (ProjectItem item in project.ProjectItems)
                 {
-                    if (item.Name == "gen")
+                    if (item.Name == "project.json")
                     {
-                        gen_folders.Add(item);
+                        imp_configs.Add(item);
                         break;
                     }
                 }
             }
 
-            foreach (var folder in gen_folders)
+            foreach (var folder in imp_configs)
             {
-               synchronize(folder);
+               process_project_config(folder);
             }
+        }
 
+        static void process_project_config(ProjectItem config_file)
+        {
+            // Load configuration
+            var project = config_file.ContainingProject;
+            var config_contents = File.ReadAllText(config_file.FileNames[0]);
+            var config = JsonConvert.DeserializeObject<Project_Configuration>(config_contents);
+            var target = config.targets[0];
+            var gen_name = target.path ?? "gen";
+            var gen = get_child_by_name(project.ProjectItems, gen_name);
+
+            // Run overlord
+            var overlord = new Overlord(target.type);
+            var project_path = Path.GetDirectoryName(project.FullName);
+            var input = Path.Combine(project_path, config.sources[0]);
+            var files = Directory.Exists(input)
+                ? Overlord.aggregate_files(input)
+                : new List<string> { input };
+            
+            var codes = files.Select(File.ReadAllText);
+            overlord.summon_many(codes);
+
+            overlord.flatten();
+            overlord.post_analyze();
+            var output_path = gen.FileNames[0];
+            Generator.clear_folder(output_path);
+            overlord.target.run(output_path);
+
+            // Synchronize generated files
+            synchronize(gen);
         }
 
         static void synchronize(ProjectItem root)
@@ -57,17 +103,16 @@ namespace Imp_VS
             foreach (var folder in folders)
             {
                 var folder_name = Path.GetFileName(folder);
-                var child_folder = get_child_by_name(root, folder_name) 
+                var child_folder = get_child_by_name(root.ProjectItems, folder_name) 
                     ?? root.ProjectItems.AddFolder(folder_name);
 
                 synchronize(child_folder);
             }
         }
 
-        static ProjectItem get_child_by_name(ProjectItem root, string name)
+        static ProjectItem get_child_by_name(ProjectItems items, string name)
         {
-            return root.ProjectItems.Cast<ProjectItem>()
-                .FirstOrDefault(item => item.Name == name);
+            return items.Cast<ProjectItem>().FirstOrDefault(item => item.Name == name);
         }
     }
 }
